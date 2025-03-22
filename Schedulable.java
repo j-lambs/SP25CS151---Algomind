a61138c252b8139f54258bd61a178a9c829ec204
McCay <mrhodebe@ucsc.edu>
2025-03-22 11:35:15 -0700

    Updated the constructor to match the session updates.

--- Schedulable.java
+++ Schedulable.java
@@ -2,6 +2,6 @@ import java.util.BitSet;
 
 interface Schedulable {
     void setAvailability(int timeStart, int duration, boolean isAvailable);
-    boolean isAvailable(int startTime, int duration);
+    boolean isAvailable(int startTime, int duration, Tutors tutor, Student student);
     BitSet getAvailabilityBitSet(BitSet newSchedule);
 }
